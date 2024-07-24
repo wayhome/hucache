7431bff484f02e43ffc15f7226c47d5e5ab467f4
Young King <yanckin@gmail.com>
2024-07-24 11:46:20 +0800

    clean code

--- hucache/__init__.py
+++ hucache/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .cache import CacheFactory
+
+__all__ = ["CacheFactory"]
--- hucache/serializer.py
+++ hucache/serializer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-提供主要用于数据交换的序列化处理机制。
+Provides serialization mechanisms primarily for data exchange.
 """
 
 __all__ = ["Serializer"]
@@ -10,15 +10,19 @@ import datetime
 import decimal
 import json
 import logging
-import six
+from typing import Any, Union
+
 from packaging import version
+import sqlalchemy
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.engine import ResultProxy
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
-    logging.warning("can't import cpickle, use pickle instead")
+    logging.warning("Unable to import cPickle, using pickle instead")
 
 
 class ObjectDict(dict):
@@ -33,146 +37,115 @@ class ObjectDict(dict):
     def __setattr__(self, name, value):
         self[name] = value
 
-    def __delattr__(self, name):
-        del self[name]
 
+class Serializer:
+    """Serialization handler"""
 
-class Serializer(object):
-    """序列化处理器"""
-
-    #: 支持的序列化格式。
     SUPPORTED_FORMATS = ["JSON", "PICKLE"]
 
-    def __init__(self, format="JSON"):
-        """创建一个序列化处理器。
+    def __init__(self, format: str = "JSON"):
+        """
+        Create a serialization handler.
 
-        :param str format: 指定该序列化处理器采用的格式，如 YAML、JSON 等。
+        :param format: Specifies the format for this serialization handler (e.g., JSON, PICKLE).
         """
         format = format.upper()
-        if format in self.SUPPORTED_FORMATS:
-            self.format = format
-        else:
-            raise ValueError("unsupported serializaion format")
+        if format not in self.SUPPORTED_FORMATS:
+            raise ValueError("Unsupported serialization format")
+        self.format = format
 
-    def load(self, stream):
-        """从参数 ``stream`` 中获取数据。
-
-        :param stream: 要载入数据的来源。可以是字符串或文件等类型。
-        :type stream: mixed
-        :rtype: str|unicode|file
+    def load(self, stream: Union[str, bytes, Any]) -> Any:
         """
-        func_name = "".join(["_from_", self.format.lower()])
-        func = globals()[func_name]
-        return func(stream)
+        Load data from the given stream.
 
-    def dump(self, data):
-        """将指定数据 ``data`` 转换为序列化后的信息。
-
-        :param data: 要序列化的数据。通常是某种映射或序列类型。
-        :type data: mixed
-        :rtype: str|unicode
+        :param stream: The source to load data from. Can be a string, bytes, or file-like object.
+        :return: Deserialized data.
         """
-        func_name = "".join(["_to_", self.format.lower()])
-        func = globals()[func_name]
-        return func(data)
+        func_name = f"_from_{self.format.lower()}"
+        return globals()[func_name](stream)
 
-    def serialize(self, data):
-        """:meth:`dump` 方法的别名。"""
-        return self.dump(data)
+    def dump(self, data: Any) -> Union[str, bytes]:
+        """
+        Convert the specified data to serialized information.
 
-    def unserialize(self, stream):
-        """:meth:`load` 方法的别名。"""
-        return self.load(stream)
+        :param data: The data to serialize. Usually a mapping or sequence type.
+        :return: Serialized data as string or bytes.
+        """
+        func_name = f"_to_{self.format.lower()}"
+        return globals()[func_name](data)
 
-    def encode(self, data):
-        """:meth:`dump` 方法的别名。"""
-        return self.dump(data)
-
-    def decode(self, stream):
-        """:meth:`load` 方法的别名。"""
-        return self.load(stream)
+    serialize = dump
+    unserialize = load
+    encode = dump
+    decode = load
 
 
-def _from_pickle(stream):
+def _from_pickle(stream: Union[str, bytes, Any]) -> Any:
     """Load data from PICKLE file or string"""
-    if isinstance(stream, six.string_types):
-        data = pickle.loads(stream)
-    else:
-        data = pickle.load(stream)
-    return data
+    if isinstance(stream, (str, bytes)):
+        return pickle.loads(stream)
+    return pickle.load(stream)
 
 
-def _to_pickle(data):
+def _to_pickle(data: Any) -> bytes:
     """Dump data into a PICKLE string."""
     return pickle.dumps(data)
 
 
-def _from_json(stream):
-    """Load data form a JSON file or string."""
-    if isinstance(stream, six.string_types) or isinstance(stream, six.binary_type):
-        data = json.loads(stream, object_hook=lambda d: ObjectDict(d))
-    else:
-        data = json.load(stream, object_hook=lambda d: ObjectDict(d))
-    return data
+def _from_json(stream: Union[str, bytes, Any]) -> Any:
+    """Load data from a JSON file or string."""
+    if isinstance(stream, (str, bytes)):
+        return json.loads(stream, object_hook=ObjectDict)
+    return json.load(stream, object_hook=ObjectDict)
 
 
-def _to_json(data):
+def _to_json(data: Any) -> str:
     """Dump data into a JSON string."""
     return json.dumps(data, cls=AwareJSONEncoder)
 
 
 class AwareJSONEncoder(json.JSONEncoder):
-    """JSONEncoder subclass that knows how to encode date/time and
-    decimal types, and also ResultProxy/RowProxy of SQLAlchemy.
+    """
+    JSONEncoder subclass that handles date/time, decimal types,
+    and SQLAlchemy's ResultProxy/RowProxy.
     """
 
     DATE_FORMAT = "%Y-%m-%d"
     TIME_FORMAT = "%H:%M:%S"
 
-    def default(self, o):
-        import sqlalchemy
-        from sqlalchemy.ext.declarative import DeclarativeMeta
-        from sqlalchemy.engine import ResultProxy
+    def default(self, o: Any) -> Any:
         sa_version = version.parse(sqlalchemy.__version__)
 
-        # Handling for SQLAlchemy versions before 1.4
         if sa_version < version.parse("1.4"):
-            from sqlalchemy.engine import ResultProxy, RowProxy
+            from sqlalchemy.engine import RowProxy
             from sqlalchemy.util._collections import AbstractKeyedTuple
 
             if isinstance(o, RowProxy):
                 return dict(o)
-            elif isinstance(o, AbstractKeyedTuple):
+            if isinstance(o, AbstractKeyedTuple):
                 return o._asdict()
-
-        # Handling for SQLAlchemy 1.4 and above
-        if sa_version >= version.parse("1.4"):
+        else:
             from sqlalchemy.engine import Row
 
             if isinstance(o, Row):
                 return dict(o)
-            elif isinstance(o, tuple) and hasattr(o, '_fields'):  # namedtuple
+            if isinstance(o, tuple) and hasattr(o, "_fields"):
                 return o._asdict()
 
         if isinstance(o, datetime.datetime):
-            # d = datetime_safe.new_datetime(o)
-            # return d.strftime("%s %s" % (self.DATE_FORMAT, self.TIME_FORMAT))
-            return o.strftime("%s %s" % (self.DATE_FORMAT, self.TIME_FORMAT))
-        elif isinstance(o, datetime.date):
-            # d = datetime_safe.new_date(o)
+            return o.strftime(f"{self.DATE_FORMAT} {self.TIME_FORMAT}")
+        if isinstance(o, datetime.date):
             return o.strftime(self.DATE_FORMAT)
-        elif isinstance(o, datetime.time):
+        if isinstance(o, datetime.time):
             return o.strftime(self.TIME_FORMAT)
-        elif isinstance(o, decimal.Decimal):
+        if isinstance(o, decimal.Decimal):
             return str(o)
-        elif isinstance(o, ResultProxy):
+        if isinstance(o, ResultProxy):
             return list(o)
-        elif isinstance(o.__class__, DeclarativeMeta):
-            fields = {}
-            instance_dict = o.__dict__
-            for field in instance_dict:
-                if not field.startswith("_"):
-                    fields[field] = instance_dict[field]
-            return fields
-        else:
-            return super(AwareJSONEncoder, self).default(o)
+        if isinstance(o.__class__, DeclarativeMeta):
+            return {
+                field: value
+                for field, value in o.__dict__.items()
+                if not field.startswith("_")
+            }
+        return super().default(o)
